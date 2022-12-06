de78efb91f2daea1678a7dd2e5ff709e06cc004e
Lasha Koroshinadze <lashakoroshinadze199@gmail.com>
2022-12-07 00:34:36 +0100

    High mem, low perf

--- main.cpp
+++ main.cpp
@@ -68,17 +68,6 @@ public:
         return bytes_to_ZZ(SHA256(input)) % N;
     }
 
-
-    ZZ trapdoor(const ZZ &g, long t) const {
-        ZZ e = PowerMod(ZZ(2), t, fi);
-        return PowerMod(g, e, N);
-    }
-
-    ZZ eval(const ZZ &g, long t) const {
-        ZZ e = power2_ZZ(t);
-        return PowerMod(g, e, N);
-    }
-
     ZZ random_prime() {
         return RandomPrime_ZZ(2 * (SECURITY_PARAM + 10), NUM_TRIALS);
     }
@@ -93,27 +82,170 @@ public:
         return NextPrime(numeric, NUM_TRIALS);
     }
 
-    ZZ trapdoor_proof(const ZZ &g, long t, const ZZ &l) {
-        ZZ e = (power2_ZZ(t) / l) % fi;
-        return PowerMod(g, e, N);
+    // Algorithm 1
+    std::pair<ZZ, ZZ> trapdoor(const ZZ &x, long t) {
+        ZZ g = hash(x);
+        ZZ e = PowerMod(ZZ(2), t, fi);
+        ZZ y = PowerMod(g, e, N);
+
+        ZZ l = noninteractive_prime(g, y);
+        ZZ rq = (power2_ZZ(t) / l) % fi;
+        ZZ pi = PowerMod(g, rq, N);
+
+        return std::make_pair(y, pi);
     }
 
-    ZZ eval_proof(const ZZ &g, long t, const ZZ &l) const {
-        ZZ x(1);
-        ZZ r(1);
-        for (long i = 0; i < t; i++) {
+    //Algorithm 2
+    bool verify(const ZZ &x, const ZZ &y, const ZZ &pi, long t) {
+        ZZ g = hash(x);
+        ZZ l = noninteractive_prime(g, y);
+        ZZ r = power2_ZZ(t) % l;
 
-        }
-        return x;
-    }
-
-    bool verify(const ZZ &g, const ZZ &pi, const ZZ &l, const ZZ &r, const ZZ &y) {
         ZZ fst = PowerMod(pi, l, N);
         ZZ snd = PowerMod(g, r, N);
         return MulMod(fst, snd, N) == y;
     }
 
+    // Algorithm 3
+    std::pair<ZZ, ZZ> eval(const ZZ &x, long t) {
+        ZZ g = hash(x);
+        ZZ y = PowerMod(g, power2_ZZ(t), N);
+
+        ZZ l = noninteractive_prime(g, y);
+        ZZ pi = simple_proof(g, l, t);
+        return std::make_pair(y, pi);
+    }
+
+    // Algorithm 4
+    ZZ simple_proof(const ZZ &g, const ZZ &l, long t) {
+        ZZ e = power2_ZZ(t) / l;
+        return PowerMod(g, e, N);
+    }
+
+    // Algorithm 4 Paper
+    ZZ simple_proof_paper(const ZZ &g, const ZZ &l, long t) {
+        ZZ x(1);
+        ZZ r(1);
+        for (long i = 0; i < t; i++) {
+            ZZ b = 2 * r / l;
+            r = 2 * r % l;
+            x = MulMod(SqrMod(x, N), PowerMod(g, b, N), N);
+        }
+        return x;
+    }
+
+    long get_block(long t, long K, long i, const ZZ &l) {
+        ZZ pwr = (power2_ZZ(K) * (power2_ZZ(t - K * (i + 1)) % l)) / l;
+        long res;
+        conv(res, pwr);
+        return res;
+    }
+
+    // Algorithm 3 & 5.1
+    std::pair<ZZ, ZZ> eval_fast_highmem(const ZZ &x, long t) {
+        long K = (long) log2((double) t) / 2;
+        std::cout<<K<<std::endl;
+        ZZ g = hash(x);
+        std::vector<ZZ> C;
+
+        ZZ y = g;
+
+        auto t01 = std::chrono::high_resolution_clock::now();
+
+        for (long i = 0; i < t; i++) {
+            if (i % K == 0) C.push_back(y);
+            y = SqrMod(y, N);
+        }
+
+        auto t02 = std::chrono::high_resolution_clock::now();
+        auto m0s = std::chrono::duration_cast<std::chrono::milliseconds>(t02 - t01);
+        std::cout << m0s.count() << std::endl;
+
+        ZZ l = noninteractive_prime(g, y);
+
+        ZZ total_power = power2_ZZ(t) / l;
+
+
+        t01 = std::chrono::high_resolution_clock::now();
+
+        ZZ pi(1);
+        for (long i = C.size() - 1; i >= 0; i--) {
+            //if (i % 10000 == 0) std::cout << i << std::endl;
+            ZZ curr_power = power2_ZZ(K * i);
+            pi = MulMod(pi, PowerMod(C[i], total_power / curr_power, N), N);
+            total_power %= curr_power;
+        }
+
+
+        t02 = std::chrono::high_resolution_clock::now();
+        m0s = std::chrono::duration_cast<std::chrono::milliseconds>(t02 - t01);
+        std::cout << m0s.count() << std::endl;
+
+        return std::make_pair(y, pi);
+
+    }
+
+    // Algorithm 3 & 5
+    std::pair<ZZ, ZZ> eval_fast_lowmem(const ZZ &x_orig, long t) {
+
+        long K = (long) log2((double) t) / 3;
+        long gamma = (long) sqrt((double) t);
+        long Kg = K * gamma;
+        long ceiling = t / Kg + (t % Kg != 0);
+
+        ZZ g = hash(x_orig);
+        std::vector<ZZ> C(ceiling + 1);
+        ZZ tmp = g;
+
+        ZZ y;
+
+        for (long i = 0; i <= t + Kg; i++) {
+            if (i == t) y = tmp;
+            if (i % Kg == 0) {
+                C[i / Kg] = tmp;
+            }
+            tmp = SqrMod(tmp, N);
+        }
+
+        ZZ l = noninteractive_prime(g, y);
+
+        long K1 = K / 2;
+        long K0 = K - K1;
+        ZZ x(1);
+        for (long j = gamma - 1; j >= 0; j--) {
+            x = PowerMod(x, power2_ZZ(K), N);
+            std::vector<ZZ> Y(1 << K);
+            for (long b = 0; b < (1 << K); b++) {
+                Y[b] = ZZ(1);
+            }
+
+            for (long i = 0; i < ceiling; i++) {
+                long b = get_block(t, K, i * gamma + j, l);
+                Y[b] = MulMod(Y[b], C[i], N);
+            }
+
+            for (long b1 = 0; b1 < (1 << K1); b1++) {
+                ZZ z = ZZ(1);
+                for (long b0 = 0; b0 < (1 << K0); b0++) {
+                    z = MulMod(z, Y[b1 * (1 << K0) + b0], N);
+                }
+                x = MulMod(x, PowerMod(z, b1 * (1 << K0), N), N);
+            }
+
+            for (long b0 = 0; b0 < (1 << K0); b0++) {
+                ZZ z = ZZ(1);
+                for (long b1 = 0; b1 < (1 << K1); b1++) {
+                    z = MulMod(z, Y[b1 * (1 << K0) + b0], N);
+                }
+                x = MulMod(x, PowerMod(z, b0, N), N);
+            }
+
+        }
+        return std::make_pair(y, x);
+    }
+
 };
+
 
 class Class_Group {
 
@@ -144,7 +276,7 @@ public:
 
 };
 
-int main() {
+int main1() {
     Class_Group cg;
     RandGen r;
     QFI g = cg.cg->random(r);
@@ -164,28 +296,41 @@ int main() {
 
 }
 
-int main1() {
+int main() {
 
     RSA_Group group;
 
     ZZ x = RandomBits_ZZ(NUM_LENGTH);
-    ZZ g = group.hash(x);
     long time = 2000000;
 
-    ZZ y = group.trapdoor(g, time);
-    std::cout << y << std::endl << std::endl;
+    //////////////////////////////////////////////////////////////////////////////
 
     auto t01 = std::chrono::high_resolution_clock::now();
-    std::cout << group.eval(g, time) << std::endl << std::endl;
+    group.trapdoor(x, time);
     auto t02 = std::chrono::high_resolution_clock::now();
     auto m0s = std::chrono::duration_cast<std::chrono::milliseconds>(t02 - t01);
     std::cout << m0s.count() << std::endl;
 
-    ZZ l = group.random_prime();
+    //////////////////////////////////////////////////////////////////////////////
 
-    ZZ pi = group.trapdoor_proof(g, time, l);
-    ZZ r = PowerMod(ZZ(2), time, l);
-    std::cout << group.verify(g, pi, l, r, y) << std::endl;
+    //////////////////////////////////////////////////////////////////////////////
 
-    return 0;
+    t01 = std::chrono::high_resolution_clock::now();
+    group.eval(x, time);
+    t02 = std::chrono::high_resolution_clock::now();
+    m0s = std::chrono::duration_cast<std::chrono::milliseconds>(t02 - t01);
+    std::cout << m0s.count() << std::endl << std::endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////////////////
+
+    t01 = std::chrono::high_resolution_clock::now();
+    group.eval_fast_highmem(x, time);
+    t02 = std::chrono::high_resolution_clock::now();
+    m0s = std::chrono::duration_cast<std::chrono::milliseconds>(t02 - t01);
+    std::cout << m0s.count() << std::endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+
 }
